19469c1328241dc003ad2cbe0d66b2db5ec451b6
yassine snaiki <yassnaiki@gmail.com>
2024-12-13 15:40:02 +0100

    footer and header

---
+++ views/partials/footer.php
@@ -0,0 +1,8 @@
+</main>
+    <footer class=" bg-green-500 h-40 w-full mt-28">
+
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
+
---
+++ views/partials/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./output.css">
+    <title>package manager</title>
+</head>
+<body class=" bg-slate-700 text-stone-100">
+    <nav class="w-full bg-green-500 py-5">
+        <div class="max-w-[1200px] flex justify-between items-center px-10 mx-auto">
+            <span class="text-base font-semibold">Package manager</span>
+            <div>
+            <?php
+             echo !isset($_SESSION['username']) ? "<a href='/login' class='font-semibold ml-6 hover:text-purple-700 transition-colors'>Login</a>" : "<a href='/logout' class='font-semibold ml-6 hover:text-purple-700 transition-colors'>Logout</a>";
+            ?>
+            </div>
+        </div>
+    </nav>
+    <main class=" min-h-[100dvh] relative">

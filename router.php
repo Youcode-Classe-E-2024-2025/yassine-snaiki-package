0b61c4813ef0a60777c74955a9cab68ff4b78f05
yassine snaiki <yassnaiki@gmail.com>
2024-12-13 15:38:06 +0100

    routing

---
+++ router.php
@@ -0,0 +1,24 @@
+<?php
+
+$routes = [
+    "/"=>"controllers/index.php",
+    "/package"=>"controllers/package.php",
+    "/login"=>"controllers/login.php",
+    "/logout"=>"controllers/logout.php",
+    "/dashboard"=>"controllers/dashboard.php",
+];
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+
+function abort($code) {
+    http_response_code($code);
+    require "controllers/404.php";
+    die();
+}
+function routeToController($routes,$uri) {
+    if(array_key_exists($uri,$routes))
+    require $routes[$uri];
+    else abort(404);
+}
+routeToController($routes,$uri);
